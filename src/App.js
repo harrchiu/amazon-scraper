06595f445395b512d98944617d8eb470bf868c7c
hc <harrchiu@gmail.com>
2021-09-20 00:55:58 -0400

    main UI, scraper connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const getScrapeResults = (curLink, setTitles, setPrices, setImages) => {
+  console.log(curLink);
+  const result = axios
+    // .get("https://price-guesser-api1.herokuapp.com/scraperTest", {
+    .post("http://localhost:5000/scraperTest", {
+      link: curLink,
+    })
+    .then((res) => {
+      setTitles(res.data.titles);
+      setPrices(res.data.prices);
+      setImages(res.data.images);
+      // console.log("here", res.data);
+    });
+};
+
+const App = () => {
+  const [titles, setTitles] = useState([]);
+  const [prices, setPrices] = useState([]);
+  const [images, setImages] = useState([]);
+
+  const [curQuery, setCurQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="page-title">Enter an Amazon item/category</div>
+      <div className="url-form">
+        <input
+          className="url-form__input"
+          placeholder="e.g. office supplies"
+          onChange={(event) => {
+            setCurQuery(event.target.value);
+          }}
+          type={"text"}
+        ></input>
+        <button
+          className="url-form__button"
+          onClick={() => {
+            setIsLoading(true);
+            console.log(isLoading);
+            const curLink = `https://www.amazon.com/s?k=${curQuery.replace(
+              " ",
+              "+"
+            )}`;
+            getScrapeResults(curLink, setTitles, setPrices, setImages);
+            setIsLoading(false);
+            console.log(isLoading);
+            console.log("title result", titles);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Scrape!
+        </button>
+      </div>
+      {isLoading && <div className="loading">loading...</div>}
+      {titles.length > 0 && (
+        <table className="results-table">
+          <tbody>
+            <tr className="results-table__header">
+              <th>#</th>
+              <th>Products</th>
+              <th>Prices</th>
+              <th>Image Links</th>
+            </tr>
+            {titles.map((a, index) => {
+              return (
+                <tr className={`results-table__result`} key={index}>
+                  <td>{index + 1}</td>
+                  <td>{titles[index]}</td>
+                  <td>{prices[index]}</td>
+                  <td>{images[index]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
-}
+};
 
 export default App;
